a8820628c2a0286465a03a3453c992fc5a816db9
SleepDevil <1752915704@qq.com>
2020-06-24 19:18:33 +0800

    完成了分类功能的开发

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import './assets/css/global.scss'
 import './plugins/element'
 import './assets/fonts/iconfont.css'
 import axios from 'axios'
+import TreeTable from 'vue-table-with-tree-grid'
 
 axios.defaults.baseURL = 'http://39.100.240.19:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
@@ -15,6 +16,8 @@ axios.interceptors.request.use(config => {
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+
+Vue.component('tree-table', TreeTable)
 
 new Vue({
   router,
